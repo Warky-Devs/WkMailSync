2b49b3029a770e87162eaf3ecfdf1b909f5bab28
Hein <hein.puth@gmail.com>
2026-03-13 23:04:30 +0200

    feat(release): improve version tagging and user prompts

--- make_release.sh
+++ make_release.sh
@@ -1,11 +1,24 @@
 #!/bin/bash
 
-# Ask if the user wants to make a release version
+# Detect the latest version tag
+latest_tag=$(git tag --sort=-v:refname | grep -E '^v[0-9]+\.[0-9]+\.[0-9]+$' | head -1)
+
+if [[ -z "$latest_tag" ]]; then
+    suggested_version="v1.0.0"
+else
+    # Increment the patch version
+    IFS='.' read -r major minor patch <<< "${latest_tag#v}"
+    suggested_version="v${major}.${minor}.$((patch + 1))"
+fi
+
+echo "Latest tag: ${latest_tag:-none}"
+echo "Suggested next version: $suggested_version"
+
 read -p "Do you want to make a release version? (y/n): " make_release
 
 if [[ $make_release =~ ^[Yy]$ ]]; then
-    # Ask the user for the version number
-    read -p "Enter the version number (e.g., v1.0.0): " version
+    read -p "Enter the version number [$suggested_version]: " version
+    version="${version:-$suggested_version}"
 
     # Validate the version number
     if ! [[ $version =~ ^(v)?([0-9]+\.){0,2}[0-9]+(\.[0-9]+)?(\.beta)?$ ]]; then
@@ -16,17 +29,27 @@ if [[ $make_release =~ ^[Yy]$ ]]; then
     # Prepend 'v' to the version if it doesn't start with it
     if ! [[ $version =~ ^v ]]; then
         version="v$version"
-    else
-        echo "Version already starts with 'v'."
     fi
 
+    # Build tag message with recent commits since last tag
+    if [[ -n "$latest_tag" ]]; then
+        commit_log=$(git log "${latest_tag}..HEAD" --oneline --no-decorate)
+    else
+        commit_log=$(git log --oneline --no-decorate -20)
+    fi
+
+    tag_message="Released $version
+
+Changes since ${latest_tag:-beginning}:
+$commit_log"
+
     # Create an annotated tag
-    git tag -a "$version" -m "Released Core $version"
+    git tag -a "$version" -m "$tag_message"
 
     # Push the tag to the remote repository
     git push origin "$version"
 
-    echo "Tag $version created for Core and pushed to the remote repository."
+    echo "Tag $version created and pushed to the remote repository."
 else
     echo "No release version created."
 fi
